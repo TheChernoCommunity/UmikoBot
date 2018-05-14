6452efe1b2104f5d0858189618e962787f90ebc0
raresica1234 <colorlord98@gmail.com>
2018-05-14 22:04:15 +0300

    Fixed message being a garbage value

--- src/modules/LevelModule.cpp
+++ src/modules/LevelModule.cpp
@@ -28,9 +28,9 @@ LevelModule::LevelModule()
 		[this](Discord::Client& client, const Discord::Message& message)
 	{
 		client.getChannel(message.channelId()).then(
-			[this, &client, &message](const Discord::Channel& channel) {
+			[this, message, &client](const Discord::Channel& channel) {
 			client.getGuildMember(channel.guildId(), message.author().id()).then(
-				[this, &client, &message, &channel](const Discord::GuildMember& member) {
+				[this, message, &channel, &client](const Discord::GuildMember& member) {
 				Discord::Embed embed;
 				QString url = "https://cdn.discordapp.com/avatars/" + QString::number(member.user().id()) + "/" + member.user().avatar() + ".png";
 				embed.setAuthor(Discord::EmbedAuthor(member.nick(), url, url));
