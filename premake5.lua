c2d027b8803ac9ffc8980dec6efa1101fbf60722
Sebastian Kylander <sebastian@kylander.me>
2018-05-28 23:03:45 +0200

    Fix collisions between premake-qt and premake-qmake

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,8 @@
-require "extensions/premake-qt/qt"
+if _ACTION:match("vs*") then
+	require "extensions/premake-qt/qt"
+elseif _ACTION == "qmake" then
+	require "extensions/premake-qmake/qmake"
+end
 
 local qtdir_x86 = io.readfile("tmp/.qtdir_x86")
 local qtdir_x64 = io.readfile("tmp/.qtdir_x64")
@@ -15,11 +19,8 @@ workspace "UmikoBot"
 	}
 
 project "UmikoBot"
-	premake.extensions.qt.enable()
 	location "sln/prj/"
 	kind "ConsoleApp"
-	qtgenerateddir "src/GeneratedFiles/"
-	qtprefix "Qt5"
 	files {
 		"src/**.cpp",
 		"src/**.h",
@@ -44,9 +45,6 @@ project "UmikoBot"
 		"widgets",
 	}
 
-	filter {"configurations:Debug"}
-		qtsuffix "d"
-
 	filter {"configurations:Release"}
 		optimize "Full"
 		defines {
@@ -56,23 +54,36 @@ project "UmikoBot"
 	filter {"platforms:x86"}
 		debugdir "res/"
 		objdir "obj/x86/"
-		qtpath (qtdir_x86)
 		targetdir "bin/x86/"
 
 	filter {"platforms:x64"}
 		debugdir "res/"
 		objdir "obj/x64/"
-		qtpath (qtdir_x64)
 		targetdir "bin/x64/"
+
+	filter {}
+
+	-- Enable premake-qt when targeting Visual Studio
+	if _ACTION:match("vs*") then
+		premake.extensions.qt.enable()
+		qtprefix "Qt5"
+		qtgenerateddir "src/GeneratedFiles/"
+
+		filter {"configurations:Debug"}
+			qtsuffix "d"
+
+		filter {"platforms:x86"}
+			qtpath (qtdir_x86)
+
+		filter {"platforms:x64"}
+			qtpath (qtdir_x64)
+	end
 
 group "QDiscord"
 
 project "QDiscordCore"
-	premake.extensions.qt.enable()
 	location "sln/prj/"
 	kind "StaticLib"
-	qtgenerateddir "dep/QDiscord/src/core/GeneratedFiles/"
-	qtprefix "Qt5"
 	files {
 		"dep/QDiscord/src/core/**.h",
 		"dep/QDiscord/src/core/**.cpp",
@@ -92,10 +103,26 @@ project "QDiscordCore"
 
 	filter {"platforms:x86"}
 		objdir "obj/x86/"
-		qtpath (qtdir_x86)
 		targetdir "bin/x86/"
 
 	filter {"platforms:x64"}
 		objdir "obj/x64/"
-		qtpath (qtdir_x64)
 		targetdir "bin/x64/"
+
+	filter {}
+
+	-- Enable premake-qt when targeting Visual Studio
+	if _ACTION:match("vs*") then
+		premake.extensions.qt.enable()
+		qtprefix "Qt5"
+		qtgenerateddir "dep/QDiscord/src/core/GeneratedFiles/"
+
+		filter {"configurations:Debug"}
+			qtsuffix "d"
+
+		filter {"platforms:x86"}
+			qtpath (qtdir_x86)
+
+		filter {"platforms:x64"}
+			qtpath (qtdir_x64)
+	end
