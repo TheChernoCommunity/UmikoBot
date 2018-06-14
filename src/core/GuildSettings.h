4494574e65118900c2a8536d3e1cb10c9b119a8e
raresica1234 <colorlord98@gmail.com>
2018-06-14 11:45:19 +0300

    Added CreateGuildSetting method and levelranks

--- src/core/GuildSettings.h
+++ src/core/GuildSettings.h
@@ -9,13 +9,23 @@
 #include <QFile>
 #include <QDir>
 
+struct LevelRank 
+{
+	QString name;
+	unsigned int minimumLevel;
+};
+
 struct GuildSetting 
 {
 	snowflake_t id;
 	QList<snowflake_t> owners;
 
-	QString prefix = "!";
+	QString prefix;
 	QList<QPair<QString, bool>> modules;
+	
+	// Level system related
+	QList<LevelRank> ranks;
+	unsigned int maximumLevel;
 };
 
 class GuildSettings 
@@ -35,4 +45,6 @@ public:
 
 	static bool IsModuleEnabled(snowflake_t guild, const QString& moduleName, bool isDefault = true);
 	static void ToggleModule(snowflake_t guild, const QString& moduleName, bool enabled, bool isDefault = true);
+private:
+	static GuildSetting CreateGuildSetting(snowflake_t id);
 };
