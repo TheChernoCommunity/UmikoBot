0b8e06458f6cd6d67844b8dd047948ada3a18415
BuildTools <colorlord98@gmail.com>
2019-12-11 21:37:24 +0200

    Created Moderation Module

---
+++ src/modules/ModerationModule.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "core/Module.h"
+
+class ModerationModule: public Module 
+{
+public:
+    ModerationModule();
+
+    void OnMessage(Discord::Client& client, const Discord::Message& message) override;
+
+    void OnSave(QJsonDocument& doc) const override;
+	void OnLoad(const QJsonDocument& doc) override;
+private:
+    bool m_invitationModeration = true;
+};
