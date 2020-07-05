85e8591145bd11eec4d1df6c79b54d4797ad0bec
Sid <techtotallyunique@gmail.com>
2020-07-06 01:29:31 +0530

    Added currency module (#11)
    * LevelModule !top Preview Changes
    
    * Updated README.md
    
    * Added currency
    
    * Bug Fixes, !gamble add-ons...
    
    * Bug Fixes and Formatting Changes
    
    * Revert Redundant Formatting Changes

--- src/modules/CurrencyModule.h
+++ src/modules/CurrencyModule.h
@@ -1,23 +1,56 @@
 #pragma once
+#include <qtimer.h>
 #include "core/Module.h"
+#include <random>
 
-class CurrencyModule : public Module
+class UmikoBot;
+
+class CurrencyModule : public Module 
 {
-public:
-	CurrencyModule();
+private:
+	struct Setting 
+	{
+		double currency;
+		bool isDailyClaimed;
+	};
 
-	void OnMessage(Discord::Client& client, const Discord::Message& message) override;
+	QMap<snowflake_t, Setting> m_settings;
 
-	void StatusCommand(QString& result, snowflake_t guild, snowflake_t user) override;
+	struct CurrencyConfig {
+		double randGiveawayProb{ 0.001 };
+		unsigned int freebieExpireTime{ 60 };	//in seconds
+		int dailyReward{ 100 };
+		int freebieReward{ 300 };
+		int gambleReward{ 50 };
+		int minGuess{ 0 };
+		int maxGuess{ 5 };
+		int gambleLoss{ 10 };
+		snowflake_t giveawayChannelId{ 0 };
+		QString currencyName;
+		QString currencySymbol;
+	}config;
+
+	std::random_device random_device;
+	std::mt19937 random_engine{ random_device() };
+	std::bernoulli_distribution randp{ config.randGiveawayProb };
+	QTimer m_timer;
+	bool isRandomGiveAwayDone{ false };
+	bool allowGiveaway{ false };
+	struct SelfGamble {
+		bool gamble{ false };
+		bool doubleOrNothing{ false };
+		snowflake_t userId{ 0 };
+		int randNum = 0;
+		snowflake_t channelId{ 0 };
+		double betAmount{ 0 }; //!Use if doubleOrNothing
+	}selfGambleData;
 
 private:
 	void OnSave(QJsonDocument& doc) const override;
 	void OnLoad(const QJsonDocument& doc) override;
 
-	struct Setting
-	{
-		int currency;
-	};
-
-	QMap<snowflake_t, Setting> m_settings;
+public:
+	CurrencyModule(UmikoBot* client);
+	void StatusCommand(QString& result, snowflake_t guild, snowflake_t user) override;
+	void OnMessage(Discord::Client& client, const Discord::Message& message) override;
 };
