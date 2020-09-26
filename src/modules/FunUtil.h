bac0fadc216a3d349d6632ed74d9e4fc1ad450c6
Sid <prollysid@pm.me>
2020-09-26 20:37:32 +0530

    Added a fun and utility module

---
+++ src/modules/FunUtil.h
@@ -0,0 +1,12 @@
+#pragma once
+// Fun and Utilities!
+#include "core/Module.h"
+#include "UmikoBot.h"
+
+class FunUtilModule : public Module, public QObject 
+{
+private:
+	
+public:
+	FunUtilModule();
+};
