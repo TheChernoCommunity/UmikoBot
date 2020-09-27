a77bf931ecadbaefc7d1dfa3ce55d8126a574766
Sahil Iyer <iyersahil12@gmail.com>
2020-09-27 17:53:19 +1000

    The bot asks questions for descriptions.

--- src/modules/UserModule.cpp
+++ src/modules/UserModule.cpp
@@ -29,18 +29,20 @@ UserModule::UserModule()
 			return;
 		}
 
-		snowflake_t victimId = mentions[0].id();
-
-		if (userDescriptions[channel.guildId()][getUserIndex(channel.guildId(), victimId)].description == "")
+		snowflake_t userId = mentions[0].id();
+		UserDescription& desc = userDescriptions[channel.guildId()][getUserIndex(channel.guildId(), userId)];
+		QString msg = formDescriptionMessage(desc);
+		
+		if (msg == "")
 		{
-			QString msg = UmikoBot::Instance().GetUsername(channel.guildId(), victimId) + " prefers an air of mystery around them...";
-			client.createMessage(message.channelId(), msg);
+			msg = UmikoBot::Instance().GetUsername(channel.guildId(), userId) + " prefers an air of mystery around them...";
 		}
 		else
 		{
-			QString msg = userDescriptions[channel.guildId()][getUserIndex(channel.guildId(), victimId)].description;
-			client.createMessage(message.channelId(), msg);
+			msg = "**" + UmikoBot::Instance().GetUsername(channel.guildId(), userId) + "'s Description**\n" + msg;
 		}
+		
+		client.createMessage(message.channelId(), msg);
 	});
 
 	RegisterCommand(Commands::USER_MODULE_I_AM, "iam",
@@ -50,27 +52,32 @@ UserModule::UserModule()
 		GuildSetting* setting = &GuildSettings::GetGuildSetting(channel.guildId());
 		QString prefix = setting->prefix;
 		snowflake_t authorId = message.author().id();
+		snowflake_t guildId = channel.guildId();
 
 		if (args.first() != prefix + "iam")
 			return;
 		
-		if (args.size() <= 1)
+		if (args.size() > 1)
 		{
 			client.createMessage(message.channelId(), "**Wrong Usage of Command!**");
 			return;
 		}
 
-		// There might be a better way to do this that I don't know of.
-		// I just need to get the rest of the message (excluding the 
-		// command) as a string
-		QString description = "**" + UmikoBot::Instance().GetUsername(channel.guildId(), authorId) + "'s Description:**\n";
-		for (int i = 1; i < args.size(); i++)
-		{
-			description += args[i] + " ";
-		}
-		userDescriptions[channel.guildId()][getUserIndex(channel.guildId(), authorId)].description = description;
-
-		client.createMessage(message.channelId(), "Description set!");
+		DescriptionData& data = guildDescriptionData[guildId];
+		data.isBeingUsed = true;
+		data.userId = authorId;
+		data.timer = nullptr; // TODO(fkp): Timer
+		data.questionUpTo = 0;
+		data.currentUserDescription = &userDescriptions[guildId][getUserIndex(guildId, authorId)];
+		data.oldUserDescription = *data.currentUserDescription;
+		
+		QString msg =
+			"**Tell me about yourself!** At any time, you can type:\n"
+			"\tskip - to skip the current question (and clear the answer)\n"
+			"\tcontinue - to skip the current question (and leave the answer)\n"
+			"\tcancel - to revert all changes\n\n";
+		msg += descriptionQuestions[0];
+		client.createMessage(message.channelId(), msg);
 	});
 }
 
@@ -85,11 +92,16 @@ void UserModule::OnSave(QJsonDocument& doc) const
 
 		for (const UserDescription& user : userDescriptions[server])
 		{
-			if (user.description == "")
+			if (formDescriptionMessage(user) == "")
 				continue;
 			
 			QJsonObject obj;
-			obj["description"] = user.description;
+			obj["name"] = user.name;
+			obj["location"] = user.location;
+			obj["industry"] = user.industry;
+			obj["programmingInterests"] = user.programmingInterests;
+			obj["currentlyWorkingOn"] = user.currentlyWorkingOn;
+			obj["githubLink"] = user.githubLink;
 
 			serverJson[QString::number(user.userId)] = obj;
 		}
@@ -117,9 +129,16 @@ void UserModule::OnLoad(const QJsonDocument& doc)
 
 		for (const QString& user : users)
 		{
+			auto userObj = obj[user].toObject();
+			
 			UserDescription description {
 				user.toULongLong(),
-				obj[user].toObject()["description"].toString(),
+				userObj["name"].toString(),
+				userObj["location"].toString(),
+				userObj["industry"].toString(),
+				userObj["programmingInterests"].toString(),
+				userObj["currentlyWorkingOn"].toString(),
+				userObj["githubLink"].toString(),
 			};
 
 			descriptions.append(description);
@@ -127,4 +146,87 @@ void UserModule::OnLoad(const QJsonDocument& doc)
 
 		userDescriptions.insert(guildId, descriptions);
 	}
+}
+
+void UserModule::OnMessage(Discord::Client& client, const Discord::Message& message)
+{
+	client.getChannel(message.channelId()).then(
+		[this, message, &client](const Discord::Channel& channel)
+		{
+			snowflake_t guildId = channel.guildId();
+			
+			if (guildId == 0 || message.author().bot())
+			{
+				// It's a bot / we're in a DM
+				return;
+			}
+
+			DescriptionData& descriptionData = guildDescriptionData[guildId];
+			snowflake_t authorId = message.author().id();
+			QString messageContent = message.content();
+			
+			if (descriptionData.isBeingUsed && descriptionData.userId == authorId)
+			{
+				QString contentToWrite = messageContent;
+				bool shouldWrite = true;
+				
+				if (messageContent == "skip")
+				{
+					contentToWrite = "";
+				}
+				else if (messageContent == "continue")
+				{
+					shouldWrite = false;
+				}
+				else if (messageContent == "cancel")
+				{
+					*descriptionData.currentUserDescription = descriptionData.oldUserDescription;
+					descriptionData.isBeingUsed = false;
+					delete descriptionData.timer;
+					return;
+				}
+
+				if (shouldWrite)
+				{
+					switch (descriptionData.questionUpTo)
+					{
+					case 0:
+						descriptionData.currentUserDescription->name = contentToWrite;
+						break;
+					case 1:
+						descriptionData.currentUserDescription->location = contentToWrite;
+						break;
+					case 2:
+						descriptionData.currentUserDescription->industry = contentToWrite;
+						break;
+					case 3:
+						descriptionData.currentUserDescription->programmingInterests = contentToWrite;
+						break;
+					case 4:
+						descriptionData.currentUserDescription->currentlyWorkingOn = contentToWrite;
+						break;
+					case 5:
+						descriptionData.currentUserDescription->githubLink = contentToWrite;
+						break;
+					}
+				}
+
+				descriptionData.questionUpTo += 1;
+
+				if (descriptionData.questionUpTo == descriptionQuestions.size())
+				{
+					client.createMessage(message.channelId(), "**All done!** Thanks for your time.");
+					descriptionData.isBeingUsed = false;
+					delete descriptionData.timer;
+					
+					return;
+				}
+
+				QString msg = descriptionQuestions[descriptionData.questionUpTo];
+				client.createMessage(message.channelId(), msg);
+			}
+		}
+	);
+
+	Module::OnMessage(client, message);
 }
--- src/modules/UserModule.h
+++ src/modules/UserModule.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <qtimer.h>
+#include <array>
 #include "core/Module.h"
 
 class UmikoBot;
@@ -11,16 +13,45 @@ public:
 
 	void OnSave(QJsonDocument& doc) const override;
 	void OnLoad(const QJsonDocument& doc) override;
-
+	void OnMessage(Discord::Client& client, const Discord::Message& message) override;
+	
 private:
 	struct UserDescription
 	{
 		snowflake_t userId;
-		QString description;
+		
+		QString name;
+		QString location;
+		QString industry;
+		QString programmingInterests;
+		QString currentlyWorkingOn;
+		QString githubLink;
 	};
 
 	QMap<snowflake_t, QList<UserDescription>> userDescriptions;
 
+	struct DescriptionData
+	{
+		bool isBeingUsed = false;
+		snowflake_t userId = 0;
+		QTimer* timer;
+		unsigned int questionUpTo = 0;
+		
+		UserDescription* currentUserDescription;
+		UserDescription oldUserDescription;
+	};
+
+	QMap<snowflake_t, DescriptionData> guildDescriptionData;
+
+	const std::array<QString, 6> descriptionQuestions = {
+		"What is your name?",
+		"Where are you from?",
+		"What industry do you work in?",
+		"What areas of programming are you interested in?",
+		"What are you currently working on?",
+		"Link to a GitHub profile:"
+	};
+	
 	snowflake_t getUserIndex(snowflake_t guild, snowflake_t id)
 	{
 		QList<UserDescription>& guildDescriptions = userDescriptions[guild];
@@ -36,5 +67,30 @@ private:
 		// If user is not added to the system, make a new one
 		guildDescriptions.append(UserDescription { id, "" });
 		return std::distance(guildDescriptions.begin(), std::prev(guildDescriptions.end()));
+	}
+
+	QString formDescriptionMessage(const UserDescription& desc) const
+	{
+		QString msg;
+
+		if (desc.name != "")
+			msg += "**Name: **" + desc.name + "\n";
+		
+		if (desc.location != "")
+			msg += "**Location: **" + desc.location + "\n";
+		
+		if (desc.industry != "")
+			msg += "**Industry: **" + desc.industry + "\n";
+		
+		if (desc.programmingInterests != "")
+			msg += "**Programming Interests: **" + desc.programmingInterests + "\n";
+		
+		if (desc.currentlyWorkingOn != "")
+			msg += "**Currently working on: **" + desc.currentlyWorkingOn + "\n";
+		
+		if (desc.githubLink != "")
+			msg += "**GitHub: **" + desc.githubLink + "\n";
+
+		return msg;
 	}
 };
