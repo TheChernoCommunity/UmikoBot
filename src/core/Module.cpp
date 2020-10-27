58579f78daa74ab6aaa33518efe6a0baf14888c0
FancyKillerPanda <iyersahil12@gmail.com>
2020-10-27 20:30:41 +1100

    VerifyAndRunAdminCmd is now a static method of UmikoBot.

--- src/core/Module.cpp
+++ src/core/Module.cpp
@@ -62,36 +62,3 @@ void Module::Load()
 		file.close();
 	}
 }
-
-void Module::VerifyAndRunAdminCmd(Discord::Client& client, const Discord::Message& message, const Discord::Channel& channel, unsigned int requiredNumberOfArgs, const QStringList& args, bool argumentShouldBeANumber, std::function<void()> callback)
-{
-	Permissions::ContainsPermission(client, channel.guildId(), message.author().id(), CommandPermission::ADMIN, [this, args, &client, message, channel, requiredNumberOfArgs, argumentShouldBeANumber, callback](bool result) 
-	{
-		if (!result) 
-		{
-			client.createMessage(message.channelId(), "**You don't have permissions to use this command.**");
-			return;
-		}
-			
-		if (args.size() != requiredNumberOfArgs) 
-		{
-			client.createMessage(message.channelId(), "**Wrong Usage of Command!** ");
-			return;
-		}
-
-		if (args.size() > 1 && argumentShouldBeANumber)
-		{
-			bool ok;
-			// Should be fine to check all numbers agains double, right?
-			args.at(1).toDouble(&ok);
-
-			if (!ok)
-			{
-				client.createMessage(message.channelId(), "**Argument is not a number!**");
-				return;
-			}
-		}
-
-		callback();
-	});
-}
