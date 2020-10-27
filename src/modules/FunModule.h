470716ecbc5285bcf92eaf7e98c9e99faa7ff0bf
Sid <prollysid@pm.me>
2020-10-27 16:03:44 +0530

    Move PollSettings constructor to its own source file

--- src/modules/FunModule.h
+++ src/modules/FunModule.h
@@ -10,8 +10,6 @@
 #include <core/Utility.h>
 #include <QtCore/QRegExp>
 
-#define EMBED_BAR_MAX_WIDTH 15.0
-
 class FunModule : public Module, public QObject 
 {
 private:
@@ -39,136 +37,7 @@ private:
 		std::shared_ptr<QTimer> timer;
 		ServerPolls polls;
 
-		PollSettings(const PollOptions& op, long long maxvotes, snowflake_t chan, const QString& name, int num, double time, const ServerPolls& pollList, snowflake_t msg) : options(op),
-			maxVotes(maxvotes), 
-			notifChannel(chan), 
-			pollName(name), 
-			pollNum(num),
-			polls(pollList),
-			pollMsg(msg)
-		{
-			timer = std::make_shared<QTimer>();
-			timer->setInterval(time * 1000);
-			timer->setSingleShot(true);
-			timer->start();
-
-			QObject::connect(timer.get(), &QTimer::timeout, 
-			[poll_num = pollNum, serverPolls = polls]() {
-
-				//! Create a message with the results
-				auto settings = serverPolls->at(poll_num);
-				auto poll_name = settings->pollName;
-				auto notif_chan = settings->notifChannel;
-				auto poll_msg = settings->pollMsg;
-				
-				std::size_t total = 0;
-				for (auto& it = settings->options.begin(); it != settings->options.end(); ++it) 
-				{
-					total += it->count;
-				}
-
-				if (total == 0) total = 1;
-
-				QList<Discord::EmbedField> fields;
-
-				for (auto& option : settings->options) 
-				{
-					double val = static_cast<double>(option.count)/ static_cast<double>(total) * EMBED_BAR_MAX_WIDTH;
-					double percentage = static_cast<double>(option.count) / static_cast<double>(total) * 100.0;
-					int num = qFloor(val);
-
-					QString str{ utility::consts::ZERO_WIDTH_SPACE };
-
-					for (int i = 0; i < num; i++) 
-					{
-						str += utility::consts::emojis::GREEN_BLOCK;
-					}
-
-					for (int i = 0; i < EMBED_BAR_MAX_WIDTH - num;i++) 
-					{
-						str += utility::consts::emojis::BLACK_BLOCK;
-					}
-
-					Discord::EmbedField field;
-					QRegExp reg{ ".+:\\d+" };
-					
-					QString desc = "";
-					QString emoji = option.emote;
-					
-					if (option.desc != "") 
-					{
-						desc = " (" + option.desc + ") ";
-					}
-
-					if (reg.exactMatch(option.emote)) 
-					{
-						if (option.isAnimated) 
-						{
-							emoji.prepend("<a:");
-						}
-						else emoji.prepend("<:");
-						emoji.append(">");
-					}
-					field.setName(emoji + desc + ": " + QString::number(percentage) + "%");
-					field.setValue(str);
-
-					fields.push_back(field);
-				}
-
-				Discord::Embed embed;
-				embed.setColor(qrand() % 16777216);
-				embed.setTitle("Results for Poll#" + QString::number(poll_num) + " " + poll_name);
-				embed.setFields(fields);
-
-
-				UmikoBot::Instance().createMessage(notif_chan, embed).then([poll_num, serverPolls](const Discord::Message& message) 
-				{
-
-					auto settings = serverPolls->at(poll_num);
-					auto poll_name = settings->pollName;
-					auto notif_chan = settings->notifChannel;
-					auto poll_msg = settings->pollMsg;
-
-					//! Get the id of the resulting message
-
-					auto resultMsg = message.id();
-
-					//! This part is used to get the guild id
-
-					UmikoBot::Instance().getChannel(notif_chan).then([poll_num, resultMsg, serverPolls](const Discord::Channel& chan) 
-					{
-
-						auto settings = serverPolls->at(poll_num);
-						auto poll_name = settings->pollName;
-						auto notif_chan = settings->notifChannel;
-						auto poll_msg = settings->pollMsg;
-
-						Discord::MessagePatch patch;
-
-						Discord::Embed embed;
-						embed.setColor(qrand() % 16777216);
-						embed.setTitle("Poll#" + QString::number(poll_num) + " " + poll_name);
-						embed.setDescription("This poll has ended. See the results [here.](https://discordapp.com/channels/" 
-							+ QString::number(chan.guildId()) + "/" 
-							+ QString::number(notif_chan) + "/" 
-							+ QString::number(resultMsg) + ")");
-
-						patch.setEmbed(embed);
-						UmikoBot::Instance().editMessage(notif_chan, poll_msg, patch);
-						UmikoBot::Instance().deleteAllReactions(chan.id(), poll_msg);
-
-						//! Remove the poll from our pending list
-						for (int i = 0; i < serverPolls->size(); i++) 
-						{
-							if (serverPolls->at(i)->pollNum == poll_num)
-							{
-								serverPolls->removeAt(i);
-							}
-						}
-					});
-				});
-			});
-		}
+		PollSettings(const PollOptions& op, long long maxvotes, snowflake_t chan, const QString& name, int num, double time, const ServerPolls& pollList, snowflake_t msg);
 	};
 	Polls m_polls;
 	
