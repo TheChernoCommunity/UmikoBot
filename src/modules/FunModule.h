9013327e28745d44a948bbc9c9f8530c6cde9564
BEASTSM96 <beastboioioi.dev@gmail.com>
2020-11-01 14:18:49 +0000

    Fixed vs thing

--- src/modules/FunModule.h
+++ src/modules/FunModule.h
@@ -10,7 +10,7 @@
 #include <core/Utility.h>
 #include <QtCore/QRegExp>
 
-class FunModule : public Module, public QObject
+class FunModule : public Module, public QObject 
 {
 private:
 	struct PollOption {
@@ -20,7 +20,7 @@ private:
 		std::size_t count = 0;
 	};
 	using PollOptions = QList<PollOption>;
-
+	
 	struct PollSettings;
 	using Poll = std::shared_ptr<PollSettings>;
 	using ServerPolls = std::shared_ptr<QList<Poll>>;
@@ -40,7 +40,7 @@ private:
 		PollSettings(const PollOptions& op, long long maxvotes, snowflake_t chan, const QString& name, int num, double time, const ServerPolls& pollList, snowflake_t msg);
 	};
 	Polls m_polls;
-
+	
 	//! A list of roles (for each server) that have been given poll 
 	//! creation access
 	QMap<snowflake_t, QList<snowflake_t>> m_pollWhitelist;
