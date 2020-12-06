fd45283746dfa47266215e1e032391a283147870
Fahim Fuad <fahimfuad.dev@gmail.com>
2020-12-06 22:35:12 +0600

    Updated the Moderation module. The "invitations" command now works!

--- src/modules/ModerationModule.cpp
+++ src/modules/ModerationModule.cpp
@@ -1,32 +1,51 @@
 #include "ModerationModule.h"
 #include "UmikoBot.h"
+#include "Core/Permissions.h"
 
 using namespace Discord;
 
 ModerationModule::ModerationModule()
-    : Module("moderation", true)
+	: Module("moderation", true)
 {
-    RegisterCommand(Commands::MODERATION_INVITATION_TOGGLE, "invitations",
+	RegisterCommand(Commands::MODERATION_INVITATION_TOGGLE, "invitations",
 		[this](Client& client, const Message& message, const Channel& channel)
-    {
+	{
 		m_invitationModeration ^= true;
 		client.createMessage(message.channelId(), m_invitationModeration ? "Invitations will be deleted!" : "Invitations won't be deleted!");
-    });
+	});
 }
 
 void ModerationModule::OnMessage(Client& client, const Message& message)
 {
+	client.getChannel(message.channelId()).then(
+	[this, message, &client](const Channel& channel)
+	{
+		if (m_invitationModeration)
+		{
+			if (message.content().contains("https://discord.gg/", Qt::CaseInsensitive))
+			{
+				auto authorID = message.author().id();
+				::Permissions::ContainsPermission(client, channel.guildId(), message.author().id(), CommandPermission::ADMIN,
+					[this, message, &client, authorID](bool result)
+				{
+					if (!result)
+					{
+						client.deleteMessage(message.channelId(), message.id());
+						client.createMessage(message.channelId(), "**<@" + QString::number(authorID) + ">, you can't publicly advertise servers!**");
+					}
+				});
+			}
+		}
+	});
+
 	Module::OnMessage(client, message);
-	if (m_invitationModeration)
-		if (message.content().contains("https://discord.gg", Qt::CaseInsensitive))
-			client.deleteMessage(message.channelId(), message.id());
 }
 
 
 void ModerationModule::OnSave(QJsonDocument& doc) const 
 {
 	QJsonObject json;
- 	QJsonObject moderation;
+	QJsonObject moderation;
 
 	moderation["invitationModeration"] = m_invitationModeration;
 	json["moderation"] = moderation;
